cedaf409111fd191ec59f7b06b79cab5418bbb7e
0shubham <shubhambhuimbar12@gmail.com>
2019-04-25 19:11:45 +0530

    Adding mysite

---
+++ blog/forms.py
@@ -0,0 +1,80 @@
+from django import forms
+from .models import Post, Profile,Comment
+from django.contrib.auth.models import User
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title',
+        'body',
+
+        'status',
+        'restrict_comment',
+        )
+
+
+class UserCreateForm(forms.Form):
+    username = forms.CharField(label='')
+    password = forms.CharField(label='',widget=forms.PasswordInput)
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].label = 'Username'
+        self.fields['password'].label = 'Password'
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget= forms.PasswordInput(attrs={'placeholder':'Enter password here..'}))
+    confirm_password = forms.CharField(widget= forms.PasswordInput(attrs={'placeholder':'Confirm password here..'}))
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+        )
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError("Password Doesn't matches ")
+        else:
+            return confirm_password
+
+
+class UserEditForm(forms.ModelForm):
+    username = forms.CharField(widget= forms.TextInput(attrs={'readonly':'readonly'}))
+    email = forms.CharField(widget= forms.TextInput(attrs={'readonly':'readonly'}))
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+        )
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+class PostEditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title',
+        'body',
+
+        'status',
+        'restrict_comment',
+        )
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(label="",widget=forms.Textarea(attrs={"class":"form-control",'placeholder':'Text goes here!!','rows':'4','cols':'50'}))
+    class Meta:
+        model = Comment
+        fields =('content',)
